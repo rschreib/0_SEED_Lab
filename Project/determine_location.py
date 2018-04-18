516708e537ea3c51ad53a79f0f4962630b1d4b90
rschreib <rschreib@mymail.mines.edu>
2018-04-18 16:16:55 -0600

    auto commit

--- Project/determine_location.py
+++ Project/determine_location.py
@@ -1,4 +1,11 @@
-import math # math.degrees(math.atan(math.sqrt(3)/1))
+import math # Example: math.degrees(math.atan(math.sqrt(3)/1))
+from pprint import pprint
+# import sys
+# color = sys.stdout.shell
+# try: color = sys.stdout.shell
+# except AttributeError: raise RuntimeError("Use IDLE")
+
+# Functions
 def magnitude(x, y):
     return math.sqrt(x*x + y*y)
 def feet_to_cm(feet):
@@ -7,19 +14,34 @@ def cm_to_feet(cm):
     return cm/12.0/2.54
 class Point:
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.x = float(x)
+        self.y = float(y)
     def __repr__(self):
-        return "(x,y) = (%.2f,%.2f)cm   \t(%.2f,%.2f)ft" % (self.x,self.y,cm_to_feet(self.x),cm_to_feet(self.y))
+        return("(%.2f,%.2f) ft" % (self.x,self.y))
 class Beacon:
     car = Point(0,0)
-    def __init__(self,x, y, radius = 0):
+    def __init__(self, color, name, x, y, radius = 0, angle = 0):
         self.x = x                      # Location of Beacon on Grid
         self.y = y                      # Location of Beacon on Grid
-        self.radius = magnitude(self.car.x - x, self.car.y - y)     # distance from car
-        self.angle = math.degrees(math.atan2(self.car.x - x, self.car.y - y))
+        if radius == 0:
+            self.radius = magnitude(self.car.x - x, self.car.y - y)     # distance from car
+        else:
+            self.radius = radius
+        if angle == 0:
+            self.angle = math.degrees(math.atan2(self.car.x - x, self.car.y - y))
+        else:
+            self.angle = angle
+        self.color = color
+        self.name = name
+        self.highlight = False
     def __repr__(self):
-        return "(x,y) = (%.1f,%.1f)cm   \t(%.1f,%.1f)ft\t%.1f ft\t%.1f deg\n" % (self.x,self.y,cm_to_feet(self.x),cm_to_feet(self.y),cm_to_feet(self.radius),self.angle)
+        if self.name == 'Car':
+            # return ("\033[1;35;40m%s\t%s\t(%.1f,%.1f) ft\t%.1f ft\t%.1f deg\033[0m\t\t<-CAR-" % (self.color,self.name,self.x,self.y,self.radius,self.angle))
+            return ("%s\t%s\t(%.1f,%.1f) ft\t%.1f ft\t%.1f deg\t\t<-CAR-" % (self.color,self.name,self.x,self.y,self.radius,self.angle))
+        elif self.highlight == True:
+            # return ("\033[1;31;40m%s\t%s\t(%.1f,%.1f) ft\t%.1f ft\t%.1f deg\033[0m\t<-----" % (self.color,self.name,self.x,self.y,self.radius,self.angle))
+            return ("%s\t%s\t(%.1f,%.1f) ft\t%.1f ft\t%.1f deg\t<-----" % (self.color,self.name,self.x,self.y,self.radius,self.angle))
+        return ("%s\t%s\t(%.1f,%.1f) ft\t%.1f ft\t%.1f deg" % (self.color,self.name,self.x,self.y,self.radius,self.angle))
 def get_angle_and_distance(p, offset_angle):
     angle = math.degrees(math.atan(p.y / p.x)) - 90 + offset_angle
     if angle < -90: ##########################################################################################################################  Issues
@@ -32,7 +54,6 @@ def distance_to_travel(car_Location, destination):
     dx = destination.x - car_Location.x
     dy = car_Location.y - destination.y
     return Point(dx, dy)
-
 def Intersect_Points(b1, b2):
     # https://gamedev.stackexchange.com/questions/7172/haow-do-i-find-the-intersections-between-colliding-circles
     # Determines the points at which two circles intersect.
@@ -50,7 +71,7 @@ def Intersect_Points(b1, b2):
     r0 = b1.radius
     r1 = b2.radius
 
-    print r0, r1
+    # print r0, r1
 
     d = math.sqrt((P1.real - P0.real)**2 + (P1.imag - P0.imag)**2)   # d = distance, note: d = a + b
     a = (r0**2 - r1**2 + d**2) / (2 * d)
@@ -70,11 +91,10 @@ def Intersect_Points(b1, b2):
         return car_location1
     else:
         return car_location2
+def Get_Angle_Distance_For_Arduino(beacon1,beacon2,b1_angleFromCar,xT,yT):
+    b1_y = beacon1.y
+    b1_x = beacon1.x
 
-def Get_Angle_Distance_For_Arduino(b1_x,b1_y,b1_distanceFromCar,b1_angleFromCar,b2_x,b2_y,b2_distanceFromCar,xT,yT):
-
-    beacon1 = Beacon(b1_x, b1_y, b1_distanceFromCar)
-    beacon2 = Beacon(b2_x, b2_y, b2_distanceFromCar)
     car_Location = Intersect_Points(beacon1, beacon2)
     gridAngle = 90 - math.degrees(math.atan(float(car_Location.y - b1_y)/float(b1_x - car_Location.x)))
     destination = Point(xT,yT)
@@ -85,42 +105,145 @@ def Get_Angle_Distance_For_Arduino(b1_x,b1_y,b1_distanceFromCar,b1_angleFromCar,
     if (destination.y > car_Location.y) and (destination.x > car_Location.x):
         car_travel_angle = car_travel_angle - 180
 
-    print("Beacon 1:\t{}".format(beacon1))
-    print("Beacon 2:\t{}".format(beacon2))
-    print("Car Location:\t{}".format(car_Location))
-    print("Destination:\t{}".format(destination))
-    print("Car Travel:\t{}".format(car_travel))
-    # print("Grid angle:\t%.2f" % gridAngle)
-    # print("Angle Offset:\t%.2f degrees" % angle_offset)
-    print("Angle: %.2f (%.3f)\tMagnitude: %.2f cm\t%.2f ft" % (car_travel_angle,math.radians(car_travel_angle),car_travel_distance,cm_to_feet(car_travel_distance)))
+    Beacon.car = car_Location
+    # print("\033[1;31;40m%s" % beacon1)
+    # print("%s\033[0m" % beacon2)
+    # print("Car Location:\t{}".format(car_Location))
+    # print("Destination:\t{}".format(destination))
+    # print("Car Travel:\t{}".format(car_travel))
+    #     # print("Grid angle:\t%.2f" % gridAngle)
+    #     # print("Angle Offset:\t%.2f degrees" % angle_offset)
+    # print("Angle:\t\t%.2f degrees" % car_travel_angle)
+    # print("Magnitude:\t%.2f ft" % car_travel_distance)
+    return car_travel_angle, car_travel_distance, destination
+def highlight(BeaconArray,B1color,B2color):
+    for i in range(len(BeaconArray)):
+        color = BeaconArray[i].color
+        if color == B1color or color == B2color:
+            BeaconArray[i].highlight = True
+    return(BeaconArray)
+def NextAngle(prevAngCarIsFacing, cP, nP):
+    dy = cP.y - nP.y
+    dx = nP.x - cP.x
+    angleCarWillbeFacing = math.degrees(math.atan2(dy,dx))
+    ArduinoAngleToTurn = angleCarWillbeFacing - prevAngCarIsFacing
+    return ArduinoAngleToTurn, angleCarWillbeFacing
+def NextMag(cP, nP):
+    dy = cP.y - nP.y
+    dx = nP.x - cP.x
+    return magnitude(dx,dy)
+def GetArduinoAnglesAndMagnitudes(B1color,B2color,B1hypotenuse, B2hypotenuse, B1angle, DestinationOrder):
+    # Given this
+    ColorsArray = ['NNN','NNN','NNN','NNN','NNN','RGB','RBG','GRB','GBR','BRG','BGR','RGN','RBN','GRN','GBN','BRN','BGN']
+    BeaconNames = ['Car','x1','x2','x3','x4','B1','B2','B3','B4','B5','B6','B7','B8','B9','B10','B11','B12']
+    Xarray = [4,2,7,2,6,2,5,7,9,9,9,7,4,1,0,0,0]        #Beacon X coordinates
+    Yarray = [5,2,3,7,8,0,0,0,2,5,8,11,11,11,8,5,2]     #Beacon Y coordinates
+    MagArray = [0,3.61,3.61,2.83,3.61,5.39,5.10,5.83,5.83,5.00,5.83,6.71,6.00,6.71,5.00,4.00,5.00] # Beacon Distance from Car
+    AngleArray = [0,33.7,-56.3,135.0,-146.3,21.8,-11.3,-31.0,-59.0,-90.0,-121.0,-153.4,180.0,153.4,126.9,90.0,53.1] # Beacon Angle from Car
+    BeaconArray = []
 
-    return car_travel_angle, car_travel_distance
+    # Input from PiCamera
+    B1color = 'RGB'
+    B2color = 'RBG'
 
-Xarray = [4,2,6,2,6,2,5,7,9,9,9,7,4,1,0,0,0]
-Yarray = [5,2,2,8,8,0,0,0,2,5,8,11,11,11,8,5,2]
-MagArray = [3.61,3.61,3.61,3.61,5.39,5.10,5.83,5.83,5.00,5.83,6.71,6.00,6.71,5.00,4.00,5.00]
-AngleArray = [33.7,-33.7,146.3,-146.3,21.8,-11.3,-31.0,-59.0,-90.0,-121.0,-153.4,180.0,153.4,126.9,90.0,53.1]
-BeaconArray = []
+    # Use first 2 Beacons for Creating the Grid math
+    indexB1 = ColorsArray.index(B1color)
+    indexB2 = ColorsArray.index(B2color)
 
-# hypotenuse1 = hyps[0]        # Given from piCamera in cm
-# angle1 = angles[0]           # Given from piCamera in degrees
-# hypotenuse1 = 4
-# angle1 = 45.0
+    x1 = Xarray[indexB1]
+    y1 = Yarray[indexB1]
+    B1name = BeaconNames[indexB1]
+    hypotenuse1 = MagArray[indexB1]
+    angle1 = AngleArray[indexB1]
+    # hypotenuse1 = hyps [0]        # Given from piCamera in cm
+    # angle1 = angles[0]           # Given from piCamera in degrees
 
-Beacon.car = Point(feet_to_cm(Xarray[0]),feet_to_cm(Yarray[0])) # Car Location
-for i in range(len(Xarray)):
-    x = Xarray[i]
-    y = Yarray[i]
-    BeaconArray.append(Beacon(feet_to_cm(x),feet_to_cm(y)))
+    x2 = Xarray[indexB2]
+    y2 = Yarray[indexB2]
+    B2name = BeaconNames[indexB2]
+    hypotenuse2 = MagArray[indexB2]
+    angle2 = AngleArray[indexB2]  # Do not need this, can be deleted
+    # hypotenuse2 = hyps[1]       # Given from piCamera in cm
 
-print(BeaconArray)
-# Beacon 2 information
-# x2 = feet_to_cm(2.0)        # Given from Beacon in feet
-# y2 = feet_to_cm(0.0)        # Given from Beacon in feet
-# hypotenuse2 = hyps[1]       # Given from piCamera in cm
-# hypotenuse2 = 3
+    beacon1 = Beacon(B1color,B1name,x1,y1,hypotenuse1,angle1)
+    beacon2 = Beacon(B2color,B2name,x2,y2,hypotenuse2,angle2)
 
-# Target Destination (use computer monitor coordinate graph system)
-# xT = feet_to_cm(2.0)
-# yT = feet_to_cm(4.0)
-# angleForArduino, magnitudeForArduino = Get_Angle_Distance_For_Arduino(x1,y1,hypotenuse1,angle1,x2,y2,hypotenuse2,xT,yT)
+    # Target Destination
+    xT = Xarray[DestinationOrder[0]]
+    yT = Yarray[DestinationOrder[0]]
+    xT2 = Xarray[DestinationOrder[1]]
+    yT2 = Yarray[DestinationOrder[1]]
+    xT3 = Xarray[DestinationOrder[2]]
+    yT3 = Yarray[DestinationOrder[2]]
+    xT4 = Xarray[DestinationOrder[3]]
+    yT4 = Yarray[DestinationOrder[3]]
+
+    angleForArduino, magnitudeForArduino, CarDestination = Get_Angle_Distance_For_Arduino(beacon1, beacon2,angle1,xT,yT)
+
+    Xarray[0] = Beacon.car.x
+    Yarray[0] = Beacon.car.y
+    for i in range(len(Xarray)):
+        color = ColorsArray[i]
+        name = BeaconNames[i]
+        x = Xarray[i]
+        y = Yarray[i]
+        BeaconArray.append(Beacon(color,name,x,y))
+
+    highlight(BeaconArray, B1color, B2color)
+    pprint(BeaconArray)
+
+    point1 = Point(xT,yT)
+    point2 = Point(xT2,yT2)
+    point3 = Point(xT3,yT3)
+    point4 = Point(xT4,yT4)
+
+    arduinoAngleToTurn2, angleCarWillbeFacing2 = NextAngle((angleForArduino+90), point1, point2)
+    arduinoAngleToTurn3, angleCarWillbeFacing3 = NextAngle((angleCarWillbeFacing2), point2, point3)
+    arduinoAngleToTurn4, angleCarWillbeFacing4 = NextAngle((angleCarWillbeFacing3), point3, point4)
+
+    magnitude2 = NextMag(point1, point2)
+    magnitude3 = NextMag(point2, point3)
+    magnitude4 = NextMag(point3, point4)
+
+    a1 = angleForArduino
+    a2 = arduinoAngleToTurn2
+    a3 = arduinoAngleToTurn3
+    a4 = arduinoAngleToTurn4
+
+    a = [a1,a2,a3,a4]
+    for i in range(len(a)):
+        while(a[i] > 180):
+            a[i] -= 360
+        while(a[i] < -180):
+            a[i] += 360
+
+    return  magnitudeForArduino, magnitude2, magnitude3, magnitude4, a[0], a[1], a[2], a[3], angleForArduino+90, angleCarWillbeFacing2, angleCarWillbeFacing3, angleCarWillbeFacing4
+
+# Inputs from PiCamera
+a = ['6','5','a', 'b', 'c', 'd','e','f']
+a = ''.join(a)
+a1 = a[2:-3]    # abc
+a2 = a[:2]      # 65
+a3 = a[:-2]     # 65abcd
+a4 = a[2:]      # abcdef
+print(a1,a2,a3,a4)
+B1color = 'RGB'
+B2color = 'RBG'
+B1hypotenuse = 5.39     # Given from piCamera in cm
+B2hypotenuse = 5.10     # Given from piCamera in cm
+B1angle = 21.8          # Given from piCamera in degrees
+DestinationOrder = [2,1,3,4]
+
+# color.write("Yes","STRING")
+# color.write("No\n","COMMENT")
+# color.write("Hi, are you called Miharu461? \n","KEYWORD")
+print("\nCOLOR\tITEM\tLOCATION    M_FROM_CAR  ANGLE_FROM_CAR")
+# color.write("\nCOLOR\tITEM\tLOCATION    M_FROM_CAR  ANGLE_FROM_CAR\n","KEYWORD")
+
+
+m1, m2, m3, m4, a1, a2, a3, a4, pi1, pi2, pi3, pi4  = GetArduinoAnglesAndMagnitudes(B1color, B2color, B1hypotenuse, B2hypotenuse, B1angle, DestinationOrder)
+
+print("\n\t\t\tx%d\tx%d\tx%d\tx%d\t" % (DestinationOrder[0],DestinationOrder[1],DestinationOrder[2],DestinationOrder[3]))
+print("Magnitudes (Travel):\t%.1f\t%.1f\t%.1f\t%.1f" % (m1,m2,m3,m4))
+print("Angles (Car Rotation):\t%.1f\t%.1f\t%.1f\t%.1f" % (a1,a2,a3,a4))
+print("PI Angle:\t\t%.1f\t%.1f\t%.1f\t%.1f" % (pi1,pi2,pi3,pi4))
